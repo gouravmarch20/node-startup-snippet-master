80d3260b85cc2feda6a69dc458591524bdfe81f6
Gourav <71632638+gouravmarch20@users.noreply.github.com>
2021-07-21 20:30:07 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,11 @@
 // --- npm inculuding
-const mongooose = require('mongoose');
+const mongoose = require('mongoose');
 
 //---  Schema 
 const UserSchema = new mongooose.Schema({});
 // --- mongooose methord
 UserSchema.methods.addMessage = async function () {};
 //--- collection name : User
-const User = mongooose.model('User', UserSchema);
+const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
